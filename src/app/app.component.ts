2c9da468ba091ce96a4a0ac59cb54e86e9581426
Chris C <cibucristi1@gmail.com>
2024-02-17 20:55:42 +0200

    added configs & html for the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'altv-angular';
+
+  isAlt: boolean = (window as any).isAlt;
+  devMode: boolean = environment.production;
 }
